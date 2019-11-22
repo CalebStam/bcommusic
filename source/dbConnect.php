504d45cc3e6164a7821926289e8725a4eaf8b67f
Caleb Stam <calebstam1@gmail.com>
2019-11-22 14:36:50 -0700

    added words

---
+++ source/dbConnect.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $user = "bcommusi";
+    $pass = "\",\$V5-kc}u(D";
+    $dbname = "bcommusi_music";
+    $con = new mysqli($host, $user, $pass, $dbname)
+            or die('Could not connect to the database server. ' . mysqli_connect_error());
+?>
